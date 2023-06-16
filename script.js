c6a9a63a7dd5a26026785777269f41c500fbf25a
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-16 18:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const para = document.getElementById("status");
+function Btn(){
+	para.innerHTML = '<h1>Entered Metaverse</h1>';
+}
+const button = document.getElementById("enterBtn");
+button.addEventListner(onclick,Btn())
